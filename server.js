0d85054b0233a071abc4268ef3f5f2c5fe55980e
jenshnck <jhonack@sas.upenn.edu>
2017-08-14 13:51:12 -0700

    wobble

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Require all groups of routes here
 app.use(express.static(__dirname + '/build'));
@@ -11,6 +12,6 @@ app.get('*', function(req,res){
 
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server for React Todo App listening on port 3000!')
 });
